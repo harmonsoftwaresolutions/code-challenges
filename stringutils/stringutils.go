bcc15f5aa4c06c2333d69ad5ae2d627d857ed026
Evan Harmon <evan.p.harmon@gmail.com>
2017-06-04 14:48:32 -0700

    added LetterCapitalize challenge

--- stringutils/stringutils.go
+++ stringutils/stringutils.go
@@ -4,6 +4,7 @@ package stringutils
 import (
 	"fmt"
 	"strings"
+	"unicode"
 )
 
 // ReverseString reverse a string using runes
@@ -45,4 +46,35 @@ func LongestWord(s string) string {
 	}
 
 	return arr[idx]
+}
+
+// LetterCapitalize capitalize first letter
+func LetterCapitalize(s string) string {
+	// split by space
+	arr := strings.Split(s, " ")
+	fmt.Println("array len ", len(arr))
+	// capitalize first letters
+	for i := range arr {
+		fmt.Println("i ", i)
+		fmt.Println("arr item", arr[i])
+		// fmt.Println("index check ", len(arr[i]))
+		// fmt.Println("array check ", arr[i])
+		r := []rune(arr[i])
+		fmt.Println("rune ", r)
+		fmt.Println("rune ", r[0])
+		r[0] = unicode.ToUpper(r[0])
+		arr[i] = string(r)
+	}
+	fmt.Println("arr ", arr)
+	// add spaces back in to new string
+	newStr := ""
+	for i := 0; i < len(arr); i++ {
+		if i != 0 {
+			newStr = newStr + " " + arr[i]
+		} else {
+			newStr = arr[i]
+		}
+		fmt.Println("newStr ", newStr)
+	}
+	return newStr
 }
--- stringutils/stringutils_test.go
+++ stringutils/stringutils_test.go
@@ -19,3 +19,12 @@ func TestLongestWord(t *testing.T) {
 		t.Error("Expected longest word to be enormously, got ", s)
 	}
 }
+
+func TestLetterCapitalize(t *testing.T) {
+	var s string
+	s = LetterCapitalize("this is all lower case letters")
+
+	if s != "This Is All Lower Case Letters" {
+		t.Error("expected every letter to be capitalized, got ", s)
+	}
+}
