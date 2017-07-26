a1ede8bbee3e8aba474e4d3908f4b1524eee515a
Evan Harmon <EPH3@pge.com>
2017-07-26 13:37:03 -0700

    morgan helped me make factorial recursive

--- factorials/factorials.go
+++ factorials/factorials.go
@@ -1,19 +1,12 @@
 // Package factorials challenges
 package factorials
 
-import (
-	"fmt"
-)
+import ()
 
 // FirstFactorial find the first factorial
 func FirstFactorial(num int) int {
-	fmt.Printf("Num input: %d\n", num)
-	factorial := 1
-	for i := 1; i <= num; i++ {
-		fmt.Printf("i: %v\t", i)
-		fmt.Printf("factorial: %v\t", factorial)
-		factorial = factorial * i
-		fmt.Printf("factorial: %v\n", factorial)
+	if num == 0 {
+		return 1
 	}
-	return factorial
+	return num * FirstFactorial(num-1)
 }
