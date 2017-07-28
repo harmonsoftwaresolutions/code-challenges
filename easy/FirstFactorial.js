5e6fbfeda53773116d46618022be6dca32db3932
Evan Harmon <EPH3@pge.com>
2017-07-27 17:10:43 -0700

    add description comment for firstFactorial js

--- easy/FirstFactorial.js
+++ easy/FirstFactorial.js
@@ -1,5 +1,10 @@
 const assert = require('assert');
 
+// Have the function FirstFactorial(num) take the num parameter being passed
+// and return the factorial of it (e.g. if num = 4, return (4 * 3 * 2 * 1)).
+// For the test cases, the range will be between 1 and 18 and the input will
+// always be an integer. 
+
 function FirstFactorial(num) { 
   if (num === 0) {
       return 1;
