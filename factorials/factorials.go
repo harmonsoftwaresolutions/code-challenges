15cc685a1e91fd9b7b0cdfeacf9d7f06be5586de
Evan Harmon <evan.p.harmon@gmail.com>
2017-07-27 17:11:42 -0700

    add description comment for firstFactorial go

--- factorials/factorials.go
+++ factorials/factorials.go
@@ -3,6 +3,11 @@ package factorials
 
 import ()
 
+// Have the function FirstFactorial(num) take the num parameter being passed
+// and return the factorial of it (e.g. if num = 4, return (4 * 3 * 2 * 1)).
+// For the test cases, the range will be between 1 and 18 and the input will
+// always be an integer.
+
 // FirstFactorial find the first factorial
 func FirstFactorial(num int) int {
 	if num == 0 {
