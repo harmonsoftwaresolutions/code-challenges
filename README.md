0fffa77ebabca8e131f32ce448d624b493166803
Evan Harmon <evan.harmon@levvel.io>
2017-08-20 14:51:05 -0700

    edit swapcase challenge comment in py

--- README.md
+++ README.md
@@ -148,3 +148,15 @@ Output:"bcdeeorty"
 
 Input:"hooplah"
 Output:"ahhloop"
+
+## Swap Case
+Have the function SwapCase(str) take the str parameter and swap the case
+of each character. For example: if str is "Hello World" the output should
+be hELLO wORLD. Let numbers and symbols stay the way they are.
+
+### Test Cases:
+Input:"Hello-LOL"
+Output:"hELLO-lol"
+
+Input:"Sup DUDE!!?"
+Output:"sUP dude!!?"
--- swapcase.py
+++ swapcase.py
@@ -1,4 +1,6 @@
-# have the function SwapCase(str) take the str parameter and swap the case of each character. For example: if str is "Hello World" the output should be hELLO wORLD. Let numbers and symbols stay the way they are.
+#  Have the function SwapCase(str) take the str parameter and swap the case
+#  of each character. For example: if str is "Hello World" the output should
+#  be hELLO wORLD. Let numbers and symbols stay the way they are.
 
 def SwapCase(s):
 	li = list(s)
