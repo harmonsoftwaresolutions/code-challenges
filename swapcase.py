d5580728bb84b70b76eec9dc1a237995d8f552d4
Evan Harmon <evan.harmon@levvel.io>
2017-08-20 14:48:34 -0700

    add swapcase challenge in py

---
+++ swapcase.py
@@ -0,0 +1,16 @@
+# have the function SwapCase(str) take the str parameter and swap the case of each character. For example: if str is "Hello World" the output should be hELLO wORLD. Let numbers and symbols stay the way they are.
+
+def SwapCase(s):
+	li = list(s)
+	newli = []
+	for c in li:
+		a = c
+		if c.isalpha():
+			a = c.swapcase()
+		newli.append(a)
+	s = ''.join(newli)
+	return s
+
+def test1():
+	assert SwapCase("Hello-LOL") == "hELLO-lol"
+	assert SwapCase("Sup DUDE!!?") == "sUP dude!!?"
